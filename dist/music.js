a8d35c4a60967aa8bcad7f99d3ef3fece9d073a4
River-South <1469977622@qq.com>
2021-12-09 02:12:54 +0800

    Site updated: 2021-12-09 02:12:54

---
+++ dist/music.js
@@ -0,0 +1,14 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: 'Beauty and Beast',
+        artist: '手嶌葵',
+        url: '手嶌葵 - Beauty And The Beast.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      }
+    ]
+});
+
